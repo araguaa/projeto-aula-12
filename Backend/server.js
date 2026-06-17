d45d2cd5ecbda32b8af371a9074b43f8d6d87966
araguaa <jafesones@gmail.com>
2026-06-17 04:21:13 -0300

    preparando deploy

--- Backend/server.js
+++ Backend/server.js
@@ -11,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/users', userRoutes);
 
-app.listen(3000, () => {
-  console.log('Servidor rodando em http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
