12e02a7ed8419c9e66077afc8e73c65eb3c0775e
araguaa <jafesones@gmail.com>
2026-06-17 04:33:50 -0300

    arrumando server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,8 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/users', userRoutes);
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
 });
